5db9454519de3caa3bd780208ee1484b2a3968ef
Nils Rugsveen <nirugsveen@gmail.com>
2018-04-25 18:41:28 +0200

    Add exercises

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -6,12 +6,23 @@ import (
 	"fmt"
 )
 
+var todos = []string{
+	"Commit",
+	"Clean",
+	"Sleep",
+}
+
 func MakeRouter() *mux.Router {
 	router := mux.NewRouter()
-	router.HandleFunc("/hello", helloWorld).Methods("GET")
+	router.HandleFunc("/todo/0", GetTodo).Methods("GET")
+
+	// FIXME: Create endpoint to return all todos
+
+	// FIXME: Create endpoint to remove the first todo in the list, and return the remaining todos
+
 	return router
 }
 
-func helloWorld(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(w, "Hello world!")
+func GetTodo(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprint(w, todos[0])
 }
